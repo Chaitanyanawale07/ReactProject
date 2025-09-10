6bc0034e0e3e7772ac0953c017be9f649301d090
Chaitanyanawale07 <Chaitanayanawale7@gmail.com>
2025-09-10 09:45:13 +0530

    File Added

--- src/hooks/Counter_arrow.js
+++ src/hooks/Counter_arrow.js
@@ -20,7 +20,7 @@ import { useEffect } from "react";
    return(<div>
     <h3>React demo with useState and useEffect hook</h3>
     <h2>{count}</h2>
-    <button onClick={()=>setCounter(c1+1)}>count {c1}</button>
+    <button onClick={()=>setCounter(c1+1)}>count   {c1}</button>
    </div>)
  }
  export default Counter_arrow;
